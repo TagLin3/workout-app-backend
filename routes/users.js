625bd9ea38a96f5d5b45e99b021a318b693bdcb2
TagLin3 <teemu.v.lahteinen@gmail.com>
2024-08-15 19:16:31 +0300

    implemented a route for changing a user's password

--- routes/users.js
+++ routes/users.js
@@ -30,12 +30,28 @@ userRouter.put("/:id", async (req, res) => {
   if (req.user.id !== req.params.id) {
     return res.status(404).json({ error: "User not found or you don't have access to edit this user." });
   }
-  const updatedUser = User.findOneAndUpdate(
+  const userToUpdate = await User.findById(req.params.id);
+  const updatedUser = await User.findOneAndUpdate(
     { _id: req.params.id },
-    req.body,
-    { new: true },
+    {
+      name: userToUpdate.name === req.body.name ? undefined : req.body.name,
+      username: userToUpdate.username === req.body.username ? undefined : req.body.username,
+    },
+    { new: true, runValidators: true },
   );
   return res.status(200).json(updatedUser);
+});
+
+userRouter.put("/:id/changePassword", async (req, res) => {
+  const { oldPassword, newPassword } = req.body;
+  const userToChangePasswordFor = await User.findById(req.params.id);
+  const passwordCorrect = await bcrypt.compare(oldPassword, userToChangePasswordFor.passwordHash);
+  if (!passwordCorrect) {
+    return res.status(401).json({ error: "Old password is incorrect" });
+  }
+  const newPasswordHash = await bcrypt.hash(newPassword, 10);
+  await User.updateOne({ _id: req.params.id }, { passwordHash: newPasswordHash });
+  return res.status(200).end();
 });
 
 module.exports = userRouter;
